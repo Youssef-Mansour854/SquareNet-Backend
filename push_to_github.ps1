09efb81ea4b00db939efca8938bf96548f7b0cfd
Youssef-Mansour <114363242+Youssef-Mansour854@users.noreply.github.com>
2026-05-05 17:04:56 +0300

    Cleanup: Remove local upload scripts

--- push_to_github.ps1
+++
@@ -1,7 +0,0 @@
-# GitHub Upload Script for SquareNet-Backend
-git init
-git add .
-git commit -m "Initial commit: SquareNet Backend structure"
-git branch -M main
-git remote add origin https://github.com/Youssef-Mansour854/SquareNet-Backend.git
-git push -u origin main
