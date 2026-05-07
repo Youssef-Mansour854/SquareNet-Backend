52ca5dc1416945f58b4763546a5faa44eeda2c8f
Youssef-Mansour <114363242+Youssef-Mansour854@users.noreply.github.com>
2026-05-07 19:15:40 +0300

    Final backend updates for production

---
+++ promote_admin.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const User = require('./models/userModel');
+
+dotenv.config({ path: 'config.env' });
+
+const promote = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log('Connected to Database...');
+
+    const email = 'yh80984@gmail.com'; 
+    const user = await User.findOneAndUpdate(
+      { email },
+      { role: 'superadmin' },
+      { new: true }
+    );
+
+    if (user) {
+      console.log(`Success! User ${email} is now a superadmin.`);
+    } else {
+      console.log('User not found. Make sure you registered this email first.');
+    }
+
+    process.exit();
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+promote();
